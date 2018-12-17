88cb320999b89fac83d6c52a24b0c16f3fe19d64
Dato <davit.bumbeishvili@gmail.com>
2018-12-17 16:00:03 +0400

    Add unicode flag

--- src/search.js
+++ src/search.js
@@ -3,15 +3,15 @@ const search = {};
 search.getSearchQueryObject = function(fname, lname, city) {
 	const result = {};
 	if (fname != '_') {
-		result.nameGe = new RegExp(`.*${fname.toLowerCase()}.*`, "i");
+		result.nameGe = new RegExp(`.*${fname.toLowerCase()}.*`, "u");
 	}
 	if (lname != '_') {
-		result.lastNameGe = new RegExp(`.*${lname.toLowerCase()}.*`, "i");
+		result.lastNameGe = new RegExp(`.*${lname.toLowerCase()}.*`, "u");
 	}
 	if (!isNaN(+city)) {
 		result.place = +city;
 	} else {
-		if (!result.nameEn && !result.lastNameEn) {
+		if (!result.nameGe && !result.lastNameGe) {
 			result.place = Math.round(Math.random() * 10);
 		}
 	}
