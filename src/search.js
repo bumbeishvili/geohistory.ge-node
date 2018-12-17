fdb8eb6d6668d15e8a06e69b0c79f837afab8512
Dato <davit.bumbeishvili@gmail.com>
2018-12-17 15:47:49 +0400

    search in georgian data

--- src/search.js
+++ src/search.js
@@ -3,10 +3,10 @@ const search = {};
 search.getSearchQueryObject = function(fname, lname, city) {
 	const result = {};
 	if (fname != '_') {
-		result.nameEn = new RegExp(`.*${fname.toLowerCase()}.*`, "i");
+		result.nameGe = new RegExp(`.*${fname.toLowerCase()}.*`, "i");
 	}
 	if (lname != '_') {
-		result.lastNameEn = new RegExp(`.*${lname.toLowerCase()}.*`, "i");
+		result.lastNameGe = new RegExp(`.*${lname.toLowerCase()}.*`, "i");
 	}
 	if (!isNaN(+city)) {
 		result.place = +city;
